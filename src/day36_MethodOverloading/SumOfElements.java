395392eb3835bd61b3c8be2eda29596d9e7f1dc2
ErdiKNT <erdi_kant_@hotmail.com>
2022-03-26 12:03:58 -0400

    day2

--- src/day36_MethodOverloading/SumOfElements.java
+++ src/day36_MethodOverloading/SumOfElements.java
@@ -1,4 +1,18 @@
 package day36_MethodOverloading;
 
 public class SumOfElements {
+    public static void main(String[] args){
+        int [] arr = {1, 5, 6, 2};
+        System.out.println(sum(arr));
+
+        System.out.println(sum(4, 6, 2, 6, 12, 9));
+    }
+
+    public static int sum(int ... nums){
+        int sum = 0;
+        for(int each: nums){
+            sum += each;
+        }
+        return sum;
+    }
 }
