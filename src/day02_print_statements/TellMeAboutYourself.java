3890376652ae96e97469b38a6ce10c41d2dbb1ce
ErdiKNT <erdi_kant_@hotmail.com>
2022-03-25 20:45:35 -0400

    These are going to be ignored.

---
+++ src/day02_print_statements/TellMeAboutYourself.java
@@ -0,0 +1,13 @@
+package day02_print_statements;
+
+public class TellMeAboutYourself {
+    public static void main(String[] args) {
+        System.out.println(" My name is ErdI KANT.\n I live in NYC. \n My hobby is watching science documentary.");
+
+    }
+}
+/*
+ShortCuts:
+ - Printing: sout.
+ - Main method: main.
+ */
