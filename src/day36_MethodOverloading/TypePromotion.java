02da351fe7c8ede801eb255f8900f1062372a9fe
ErdiKNT <erdi_kant_@hotmail.com>
2022-03-26 12:40:25 -0400

    day2

--- src/day36_MethodOverloading/TypePromotion.java
+++ src/day36_MethodOverloading/TypePromotion.java
@@ -1,2 +1,24 @@
-package day36_MethodOverloading;public class TypePromotion {
+package day36_MethodOverloading;
+
+public class TypePromotion {
+
+    public static void main(String[] args) {
+       use(3);
+       use(3.1);
+       use(3f);
+       use(3L);
+    }
+
+    public static void use(float f){
+        System.out.println("Calling float method");
+    }
+
+    public static void use(double d){
+        System.out.println("Calling double method");
+    }
+
+    public static void use(long l){
+        System.out.println("Calling long method");
+    }
+
 }
