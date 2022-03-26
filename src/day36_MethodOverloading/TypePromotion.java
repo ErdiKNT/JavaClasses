3943d9c73e72e318a391f2e624a7ed2d9b678a4d
ErdiKNT <erdi_kant_@hotmail.com>
2022-03-26 12:47:09 -0400

    day2

--- src/day36_MethodOverloading/TypePromotion.java
+++ src/day36_MethodOverloading/TypePromotion.java
@@ -1,5 +1,10 @@
 package day36_MethodOverloading;
-
+/*
+Method overloading allows methods to be created with the same name
+The parameters must be different (Number of parameters, or the types of the parameters)
+The return types should be the same, but if a method is overloaded it can be changed
+Improve re-usability and readability
+ */
 public class TypePromotion {
 
     public static void main(String[] args) {
@@ -7,6 +12,12 @@ public class TypePromotion {
        use(3.1);
        use(3f);
        use(3L);
+       use((short)9);
+
+       use((double)3);
+       short s = 5;
+       use(s);
+
     }
 
     public static void use(float f){
