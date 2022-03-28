34043a3ab50f0e461d33ed5c3fab5510986d4dc5
ErdiKNT <erdi_kant_@hotmail.com>
2022-03-28 13:50:13 -0400

    day37

---
+++ src/day37_Wrapper_ArrayList/ValueVSParse.java
@@ -0,0 +1,22 @@
+package day37_Wrapper_ArrayList;
+
+public class ValueVSParse {
+    public static void main(String[] args) {
+        String s = "30";
+
+        int seconds = Integer.parseInt(s); // Converts the "30" String into an int of 30. [Unboxing]
+
+        Integer i = Integer.valueOf(s); // Converts the "30" String into the Integer wrapper class object. [AutoBoxing]
+        System.out.println(i); // Object 30
+
+        int i2 = Integer.valueOf(s); //[We are unboxing here]
+        System.out.println(i2); // Primitive type 30
+
+        Integer i3 = Integer.parseInt(s);
+        //Integer.parseInt(s); --> Converts the "30" String into the Integer object.
+
+        String s2  = String.valueOf(30);
+        System.out.println(s2 + 5); // 305
+
+    }
+}
