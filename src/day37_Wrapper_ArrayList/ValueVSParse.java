7e805821f1bb47608c88e94eddab0fb46640c9af
ErdiKNT <erdi_kant_@hotmail.com>
2022-03-30 21:47:29 -0400

    day 37

--- src/day37_Wrapper_ArrayList/ValueVSParse.java
+++ src/day37_Wrapper_ArrayList/ValueVSParse.java
@@ -3,12 +3,11 @@ package day37_Wrapper_ArrayList;
 public class ValueVSParse {
     public static void main(String[] args) {
         String s = "30";
-
         int seconds = Integer.parseInt(s); // Converts the "30" String into an int of 30. [Unboxing]
 
         Integer i = Integer.valueOf(s); // Converts the "30" String into the Integer wrapper class object. [AutoBoxing]
-        System.out.println(i); // Object 30
-
+        System.out.println(i); // Object 30. However, valueOf(String) returns a new Integer() object whereas parseInt(String) returns a primitive int.
+        // Do not forget primitive types does not work with ArrayList but objects does.
         int i2 = Integer.valueOf(s); //[We are unboxing here]
         System.out.println(i2); // Primitive type 30
 
