d94e33702f4669653eca004f12de49971d9775ed
ErdiKNT <erdi_kant_@hotmail.com>
2022-03-31 15:59:42 -0400

    day 37

--- src/day37_Wrapper_ArrayList/WrapperClassObject.java
+++ src/day37_Wrapper_ArrayList/WrapperClassObject.java
@@ -7,15 +7,17 @@ public class WrapperClassObject {
         // What can`t we do with this: a.method()
 
         Integer a2 = new Integer(10);
-        Integer a3 = 20; //Autoboxing - primitive --> Object
+        Integer a3 = 20; //Autoboxing - primitive --> Object // Now we can apply some methods.
         int a4 = a3; // unboxing - object --> Primitive
 
-        System.out.println(a);
-        System.out.println(a2);
-        System.out.println(a3);
+        System.out.println(a); // 5
+        System.out.println(a2);// 10
+        System.out.println(a3);// 20
 
         byte b = 2;
         Byte b2 = 20;
+        b2.toString(); // For example: Now we can apply toString Method at the b2. Then we are able to make it string.
+        System.out.println(b2.toString() + 2); // The result is 202 because b2 is a string object.
 
 
 
