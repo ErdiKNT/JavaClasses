66f4534f97f48965a4572d249f2f3897b7394dce
ErdiKNT <erdi_kant_@hotmail.com>
2022-04-03 12:47:21 -0400

    day 40

--- src/day40_ArrayList/JobTitles.java
+++ src/day40_ArrayList/JobTitles.java
@@ -5,8 +5,42 @@ import java.util.Arrays;
 
 public class JobTitles {
     public static void main(String[] args) {
+        //ADD ALL
         ArrayList<String> allJobs = new ArrayList<>();
-        allJobs.addAll(Arrays.asList("SDET", "Developer","PO", "CEO", "Dev op", "QA", "BA" , "Chef" ,"Doctor","Police Officer"));
+        allJobs.addAll(Arrays.asList("SDET", "Developer","PO", "CEO", "Dev op", "QA", "BA" , "Chef" ,"Doctor","Police Officer","PO"));
+
+        //REMOVE ALL
+        ArrayList<String> jobs1 = new ArrayList<>(allJobs);
+        jobs1.removeAll(Arrays.asList("PO", "QA", "BA"));
+        System.out.println(jobs1); // [SDET, Developer, CEO, Dev op, Chef, Doctor, Police Officer]
+
+        //RETAIN ALL
+        ArrayList<String> jobs2 = new ArrayList<>(allJobs);
+        jobs2.retainAll(Arrays.asList("SDET", "Developer","Dev op"));
+        System.out.println(jobs2); // [SDET, Developer, Dev op]
+
+        //CONTAINS ALL (Returns boolean)
+        ArrayList<String> jobs3 = new ArrayList<>(allJobs);
+        System.out.println(jobs3.containsAll(Arrays.asList("Chef", "CEO")));    // True
+        System.out.println(jobs3.containsAll(Arrays.asList("Doctor", "Nurse")));// False
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
