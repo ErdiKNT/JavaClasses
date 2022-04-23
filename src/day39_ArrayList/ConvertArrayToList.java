d1029b98fbc1c8cec606151bb090a553378e6842
ErdiKNT <erdi_kant_@hotmail.com>
2022-04-23 00:07:06 -0400

    day 39

--- src/day39_ArrayList/ConvertArrayToList.java
+++ src/day39_ArrayList/ConvertArrayToList.java
@@ -2,6 +2,7 @@ package day39_ArrayList;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class ConvertArrayToList {
     public static void main(String[] args) {
